c4abe048d88fe51c495af1d3af7601c1a7388a65
Goksu Okar <goksuokar@gmail.com>
2022-10-31 19:32:59 -1000

    rework, without color

--- src/sortingAlgorithm.js
+++ src/sortingAlgorithm.js
@@ -1,23 +1,20 @@
 import { sleep } from './helpers.js';
 
-async function sorter (array, startIdx, endIdx, t = 20, states) {
+async function sorter (array, startIdx, endIdx, states) {
+  if (startIdx >= endIdx) {
+    return
+  }
+  await partition(array, startIdx, endIdx, states);
+}
+
+async function partition(array, startIdx, endIdx, states) {
   let l = startIdx;
   let r = endIdx - 1;
   let pivot = endIdx;
 
-
-
-  if (startIdx >= endIdx) {
-    return
-  }
-
   while (l <= r) {
     if (array[l] > array[pivot] && array[r] < array[pivot]) {
-      states[l] = 0;
-      states[r] = 2;
-      await swap(l, r, array, t, states);
-      states[l] = -1;
-      states[r] = -1;
+      await swap(l, r, array, states);
     }
     if (array[l] <= array[pivot]) {
       l = l + 1;
@@ -25,20 +22,23 @@ async function sorter (array, startIdx, endIdx, t = 20, states) {
     if (array[r] >= array[pivot]) {
       r = r - 1;
     }
-    await sleep(t);
   }
 
-  await swap(l, pivot, array, t, states);
+  await swap(l, pivot, array, states);
   const leftSubArrayIsSmaller = r - startIdx < l + 1 - pivot;
-  if (leftSubArrayIsSmaller) {
-    await Promise.all([sorter(array, 0, r, t, states), sorter(array, l + 1, endIdx,t, states)]);
-  } else {
-    await Promise.all([sorter(array, l + 1, endIdx, t, states), sorter(array, 0, r, t, states)]);
-  }
-}
 
-async function swap(l, r, array, t = 20, states) {
-  await sleep(t);
+  //recursive part
+  if (leftSubArrayIsSmaller) {
+    await sorter(array, 0, r, states);
+    await sorter(array, l + 1, endIdx, states);
+  } else {
+    await sorter(array, l + 1, endIdx, states);
+    await sorter(array, 0, r, states);
+  }
+};
+
+async function swap(l, r, array, states) {
+ await sleep(5);
   let temp = array[r];
   array[r] = array[l];
   array[l] = temp;
